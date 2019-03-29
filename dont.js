19a23d71483bacde9cb77bcd8cf2f6a891e85a04
Genadi Samokovarov <gsamokovarov@gmail.com>
2019-03-29 13:51:28 +0200

    Ahem, typo's and fixes

--- dont.js
+++ dont.js
@@ -1,11 +1,11 @@
 setInterval(() => {
   const mergeButton = document.querySelector('.merge-message')
   if (mergeButton && mergeButton.parentNode) {
-    mergeButton.parentNode.removeChild(element)
+    mergeButton.parentNode.removeChild(mergeButton)
   }
 
   const singleCommentButton = document.querySelector('.review-simple-reply-button')
   if (singleCommentButton && singleCommentButton.parentNode) {
-    element.parentNode.removeChild(element)
+    singleCommentButton.parentNode.removeChild(singleCommentButton)
   }
 }, 500 /* ms */)
